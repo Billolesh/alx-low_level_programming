2d5530813617dcd6ef33f775b16afe0a6665667e
Billolesh <billoleshemail>
2023-03-18 00:32:47 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 /*
  *program that prints and string of chars
  */
-int main (void)
+int main(void)
 {
-  puts("Programming is like building a multilingual puzzle");
-  return(0);
+puts("Programming is like building a multilingual puzzle");
+return (0);
 }
