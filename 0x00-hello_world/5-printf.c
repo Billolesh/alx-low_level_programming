4f15a34b1ec8c887efdee72ba9232a5771b4d1ee
Billolesh <billoleshemail>
2023-03-18 01:17:13 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - program that prints an array of chars
+ *Return: 0 (pass)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
