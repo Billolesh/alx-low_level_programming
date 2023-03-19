a1019197e0af0520c01388fd70b5d0f79510b8ff
Billolesh <billoleshemail>
2023-03-19 14:01:25 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 /**
- *main - print letters
- *Return: 0 (pass)
- */
+*main - print letters
+*Return: 0 (pass)
+*/
 int main(void)
 {
-  char yes[]="abcdefghijklmnopqrstuvwxyz\n";
+char yes[] = "abcdefghijklmnopqrstuvwxyz\n";
 int i;
- 
 for (i = 0; yes[i] != '\0'; i++)
-  {
-  putchar(yes[i]);
-  }
-  return 0;
+{
+putchar(yes[i]);
+}
+return (0);
 }
