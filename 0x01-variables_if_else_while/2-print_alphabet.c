20b33713b82b11b6ffd3d210de27624f62260623
Billolesh <billoleshemail>
2023-03-19 13:51:15 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - print letters
+ *Return: 0 (pass)
+ */
+int main(void)
+{
+  char yes[]="abcdefghijklmnopqrstuvwxyz";
+int i;
+ 
+for (i = 0; yes[i] != '\0'; i++)
+  {
+  putchar(yes[i]);
+  }
+  return 0;
+}
