038ea203beb7298dd506e90717c21f7b5a8ca797
Billolesh <billoleshemail>
2023-03-20 12:18:21 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,13 +6,13 @@
 int main(void)
 {
 int d;
-for (d = 0; d <= 9; d++)
+for (d = '0'; d <= '9'; d++)
 putchar(d);
 {
-if (d != 9)
-putchar(",");
-putchar(" ");
+if (d != '9')
+putchar(',');
+putchar(' ');
 }
-putchar("\n");
+putchar('\n');
 return (0);
 }
