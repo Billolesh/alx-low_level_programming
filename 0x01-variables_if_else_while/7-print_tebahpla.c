70e1e428ad884942c691c4ac06a2ca0276ee2429
Billolesh <billoleshemail>
2023-03-19 22:35:09 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - prints out letters in reversed form
+*Return: 0 (pass)
+*/
+int main(void)
+{
+char yes[] = "zxyvwtsrqponmlkjihgfedcba";
+int i;
+for (i = 0; yes[i] != '\0'; i++)
+{
+putchar(yes[i]);
+}
+return (0);
+}
