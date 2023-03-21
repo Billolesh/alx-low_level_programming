e0b3a9bc94886bbcabd436c1aa9cbf2565153e6f
Billolesh <billoleshemail>
2023-03-21 13:03:57 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+*print_alphabet_x10 - prints the alphabet 10 times in a row
+*Return: 0 (success)
+*/
+void print_alphabet_x10(void)
+{
+char yes[] = "abcdefghijklmnopqrstuvwxyz\n";
+int i, x;
+for (x = 0; x < 0; x++)
+{
+for (i = 0; yes[i] != '\0'; i++)
+{
+_putchar(yes[i]);
+}
+}
+}
