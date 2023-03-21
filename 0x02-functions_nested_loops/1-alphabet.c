6e13e6b612900a27160225fd8220d42aa45647f0
Billolesh <billoleshemail>
2023-03-21 12:43:48 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
-*main - function that prints all the letters i small case
-*print_alphabet prints the array of ltters
+*print_alphabet - prints the array of ltters
 *Return: 0 (success)
 */
 void print_alphabet(void)
