9fb63da29c22e82db5f9b157a7cdc7f9a87b7c9f
Billolesh <billoleshemail>
2023-03-24 01:31:53 -0700

    4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+
+#include "main.h"
+/**
+*print_most_numbers - prints numbers
+*_putchar - prints item
+*Return: number
+*/
+void print_most_numbers(void)
+{
+char yes[] = "0123456789\n";
+int i;
+for (i = 0; yes[i] != '\0'; i++)
+{
+_putchar (yes[i]);
+}
+}
+
