d1b35decb265dcc02432d36171c918a1a1a68748
Billolesh <billoleshemail>
2023-03-24 01:06:03 -0700

    3-print_numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+*print_to_98 - prints all natulral numbers
+*@n: the number to start printing from
+*Return: always 0.
+*/
+void print_to_98(int n);
+{
+if (n <= 98)
+{
+for (; n <= 98; n++)
+}
+if (n == 90)
+{
+printf("&d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+}
+else
+{
+for (; n >= 98; n--)
+{
+if (n == 98)
+{
+printf("&d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d, ", n);
+}
+}
+
