6c4bedb1bc3d23c0df770d83336620ae82a5c9b5
Billolesh <billoleshemail>
2023-03-27 13:02:06 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,9 +6,20 @@
 */
 void print_rev(char *s)
 {
-for (; *s == '\0'; s--)
+int longi = 0;
+int o;
+{
+while (*s != 0)
+{
+longi++;
+s++;
+}
+s--;
+for (o = longi; o > 0; o--)
 {
 _putchar (*s);
+s--;
 }
-_putchar ('\n');
+_putchar('\n');
+}
 }
