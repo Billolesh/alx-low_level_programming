745acccee0f5befb0e3582c32dd32abfa60b4078
Billolesh <billoleshemail>
2023-04-03 10:23:43 -0700

    1-memcpy.c

--- 0x07-pointers_arrays_strings/2-memcpy.c
+++ 0x07-pointers_arrays_strings/2-memcpy.c
@@ -8,11 +8,16 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-char *p = dest;
 unsigned int i;
-for (i = 0; i < n; i++)
-{
-dest[i] = src[i];
-}
-return (p);
-}
+ i = 0;
+ while (i < n)
+   {
+     *dest = *src;
+     dest++;
+     src++;
+     i++;
+   }
+     dest = dest - i
+   return (dest);
+   }
+
