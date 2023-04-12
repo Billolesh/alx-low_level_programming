b07d7590e2ff61e4ac23bef4be2a1225c6dc1857
Billolesh <billoleshemail>
2023-04-12 11:30:19 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,10 @@
+#include <unistd.h>
+/**
+* _putchar - write the character to stdout
+* @c: the character to print
+* Return: on success 1, on error -1 and errno set appropriately
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
