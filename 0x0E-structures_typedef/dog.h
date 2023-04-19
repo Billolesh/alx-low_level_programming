5ce754c559167ed25166ea9854cf047721334d9e
Billolesh <billoleshemail>
2023-04-19 12:29:47 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+
+#ifndef MY_FUNCTION_DEF
+#define MY_FUNCTION_DEF
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*struct dog - give the info as unit
+*@name: name of the dog
+*@age: the age of the dog
+*@owner: the name of the dog
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
