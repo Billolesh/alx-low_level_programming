93a570a593c65cea9ff742a373776a7050b534a4
Billolesh <billoleshemail>
2023-04-22 05:48:46 -0700

    1-args.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,9 +7,15 @@
 int is_prime_number(int n)
 {
 if (n <= 1)
+{
 return (0);
-else if (n / n == 1 || n / 1 == n)
+}
+for (int i = 2; i <= n / 2; i++)
+{
+if (n % i == 0)
+{
+return (0);
+}
+}
 return (1);
-else
-return (0);
 }
